f9e53c7621648d7f0091ab04519848fd1da2859d
alexanderolivares13 <124226984+alexanderolivares13@users.noreply.github.com>
2023-06-11 06:56:36 -0700

    added more JS to layout variables pointing to all the pokemon data in the JSON fetch object from pokeapi, they will later be used to display content on the webpage

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,9 +13,28 @@ var randomPokeFetch = function(){
         return response.json();
     })
     .then (function (data){
-        console.log(data);
+        // the following lines point the the properties of the JSON object with the respective information for the pokemon that was searched
+        var pokemonArtwork = data.sprites.other["official-artwork"].front_default;
+        var pokemonName = "Name: " + data.name;
+        var pokemonNumber = "Pokemon Number: " + data.id;
+        // a math equation to convert the number provided to the proper unit, height/weight are represented as whole numbers multiplied by 10 instead of floating point numbers in the JSON object. (e.g 3 = .3m / 40 = 4.0 kg respectively)
+        var pokemonHeight = "Height: " + data.height * 3.937 + " in";
+        var pokemonWeight = "Weight: " + data.weight * .2204 + " lbs";
+        // A for loop to get every available ability to the pokemon and replaces any hyphens with spaces for a cleaner look
+        for (i = 0; i < data.abilities.length; i++){
+            var abilityName = data.abilities[i].ability.name;
+            console.log("Ability: " + abilityName.replace("-", " "));
+
+        }
+        // a for loop to get every base stat for the pokemon and replaces any hyphens with spaces for a cleaner look
+        for (i = 0; i < data.stats.length; i++){ 
+            var statName =  data.stats[i].stat.name;
+            var baseStat = data.stats[i].base_stat;
+            statName = statName.toUpperCase()
+            console.log(statName.replace("-", " ") + ": " + baseStat);
+        }
     })
-    // 
+    
     var giphyAPIurl = "https://api.giphy.com/v1/stickers/search?q=" + pokeValue + "&api_key=M1nneBO2F2uWYOhj8nw5UULJYWJSrSW0"
     fetch (giphyAPIurl)
     .then (function (response) {
@@ -25,9 +44,9 @@ var randomPokeFetch = function(){
         console.log(gifData);
         // The code below will be used later to generate the area that the gif will be set to on the results page
         
-        // var stickerUrl = gifData.data[0].embed_url;
-        // var pokeSticker = document.createElement('iframe');
-        // pokeSticker.setAttribute("src", stickerUrl);
+        var stickerUrl = gifData.data[0].embed_url;
+        var pokeSticker = document.createElement('iframe');
+        pokeSticker.setAttribute("src", stickerUrl);
         })
   }
 
