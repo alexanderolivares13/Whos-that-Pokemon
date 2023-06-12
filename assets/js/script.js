127ba8f5e454f407cf54c0d3bc5ba7806bdbd011
Elaine Martinez <elainemartinez@Elaines-MacBook-Air.local>
2023-06-12 16:51:21 -0700

    updated htm directions added event listeners

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+
+var searchButton= document.getElementById("#search-button")
+searchBarEl.addEventListener("click", function(pokeFetch) {
+
+
+});
+
+var surpriseButton = document.getElementsByClassName("button-random")
+surpriseButton.addEventListener("click", function (randomPokeFetch){
+
+});
+
+var nextButton = document.getElementsByClassName("#next-button")
+nextButton.addEventListener("click", function (giphyFetch) {
+
+});
