e5068716478c69ec5a48e514be54596411587a58
alexanderolivares13 <124226984+alexanderolivares13@users.noreply.github.com>
2023-06-17 17:33:23 -0700

    added more comments, and added a new fetch function to display a fun fact about the pokemon

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,7 @@ var userWindow = window.location.href.includes("search.html");
 var modalEl = $(".modal");
 var modalButton = $(".modal-close");
 var n = 0;
+var z = 0;
 var homeButton = $(".button-home");
 
 var randomPokeFetch = function(){
@@ -24,6 +25,7 @@ var pokeFetch = function(pokeValue) {
     var pokeAPIurl = "https://pokeapi.co/api/v2/pokemon/" + pokeValue;
     fetch (pokeAPIurl)
     .then (function(response){
+        // first checks to see if the response is good from the API to force the 3 different fetch functions to run in order as opposed to running asynchronously.
         if (response.status === 200){
             giphyFetch(pokeValue);
         } else if (response.status !== 200){
@@ -42,16 +44,33 @@ var giphyFetch = function(pokeValue) {
     var giphyAPIurl = "https://api.giphy.com/v1/stickers/search?q=" + pokeValue + "+pokemon&limit=5&api_key=M1nneBO2F2uWYOhj8nw5UULJYWJSrSW0";
     fetch (giphyAPIurl)
     .then (function (response) {
+        if (response.status === 200){
+            speciesFetch(pokeValue);
+        }
         return response.json();
     })
     .then (function (gifData){
         localStorage.setItem("gifData", JSON.stringify(gifData));
+    })
+}
+
+// a 3rd fetch function used to get the fun fact on the search.html page
+var speciesFetch = function(pokeValue) {
+    var speciesAPIurl = "https:/pokeapi.co/api/v2/pokemon-species/" + pokeValue
+    fetch (speciesAPIurl)
+    .then (function(response){
+        return response.json();
+    })
+    .then (function(speciesData){
+        //after the last fetch function is successfully run then the window changes
+        localStorage.setItem("speciesData", JSON.stringify(speciesData));
         window.location.href = "assets/search.html";
     })
 }
 
 var generateGif = function(){
     var gifData = JSON.parse(localStorage.getItem("gifData"));
+    // if there is no gif stickers available then the function is killed early, this is done to ensure the rest of the code is ran on the search.html page
     if (gifData.pagination.count === 0){
         return;
     } else if (gifData.data[0].embed_url){
@@ -65,19 +84,29 @@ var generateGif = function(){
 var generateInfo = function(){
     // the following lines point the the properties of the JSON object with the respective information for the pokemon that was searched
     var data = JSON.parse(localStorage.getItem("data"));
+    var speciesData = JSON.parse(localStorage.getItem("speciesData"));
+    // a for loop to ensure the fun fact is output in english
+    for (i = 0; i < speciesData.flavor_text_entries.length; i++){
+        if(speciesData.flavor_text_entries[z].language.name !== "en"){
+            z++
+        }
+    }
     var imgCreate = document.createElement("img");
     var pokemonArtwork = data.sprites.other["official-artwork"].front_default;
     var pokemonN = data.name;
-        pokemonN = pokemonN.toUpperCase();
+    pokemonN = pokemonN.toUpperCase();
     var pokemonName = "Name: " + pokemonN;
-    var pokemonNumber = "Pokemon Number: #" + data.id;
+    var pokemonNumber = "Pokemon Number: #" + data.id; 
+    var pokemonFact = speciesData.flavor_text_entries[z].flavor_text;
+        pokemonFact = "Fun Fact: " + pokemonFact.replace("\f", " ");
     // a math equation to convert the number provided to the proper unit, height/weight are represented as whole numbers multiplied by 10 instead of floating point numbers in the JSON object. (e.g 3 = .3m / 40 = 4.0 kg respectively)
     var pokemonHeight = "Height: " + data.height * 3.937 + " in";
     var pokemonWeight = "Weight: " + data.weight * .2204 + " lbs";
-    var pokemonInfo = [pokemonName, pokemonNumber, pokemonHeight, pokemonWeight];
+    var pokemonInfo = [pokemonName, pokemonNumber, pokemonFact, pokemonHeight, pokemonWeight];
     imgCreate.setAttribute("src", pokemonArtwork);
     imgCreate.setAttribute("alt", "pokemon-artwork");
     imgEl.append(imgCreate);
+    // A for loop to to create new p tags for all the miscellaneous information about the pokemon denoted in the pokemonInfo array
     for(i = 0; i < pokemonInfo.length; i++){
         var lineCreate = document.createElement("p");
             lineCreate.textContent = pokemonInfo[i];
@@ -104,6 +133,7 @@ var generateInfo = function(){
             lineCreate.setAttribute("class", "pokemon-info");
             figureEl.append(lineCreate);
     }
+    //after all of the info is generated for the pokemon, the generate gif function will be called to display the gif sticker next to the info.
     generateGif();
 }
 
@@ -112,11 +142,12 @@ var surpriseFetchHandler = function(){
     randomPokeFetch();
 }
 
+// checks to see if the user is on the search.html page before running the generateInfo() function, since the generate function will dynamically create the information that will be displayed to the user on the search.html page
 if (userWindow){
     generateInfo();
 }
 
-
+// applied to the search bar submit button, it will capture the user's value and change it to lowercase before trying to fetch any data. non lowercase inputs will respond with a status of 404 from the API
 searchButton.on("click", function(event) {
     event.preventDefault();
     var pokeValue = searchBarEl.val();
@@ -126,7 +157,7 @@ searchButton.on("click", function(event) {
 
 surpriseButton.on("click", surpriseFetchHandler);
 
-
+// a simple function used to ensure the next button will display any next available gif stickers
 nextButton.on("click", function () {
     n++;
     var gifData = JSON.parse(localStorage.getItem("gifData"));
